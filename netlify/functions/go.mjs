9564b5ab96157dd309045e46fece30274a432568
kassis28-coder <kassis28@gmail.com>
2026-06-27 17:13:28 -0400

    Add redirect function for Amazon and other URLs

---
+++ netlify/functions/go.mjs
@@ -0,0 +1,26 @@
+export default async (req) => {
+  const url = new URL(req.url);
+  const asin = url.searchParams.get('asin');
+  const dest = url.searchParams.get('url');
+
+  let redirectUrl;
+
+  if (asin) {
+    redirectUrl = `https://www.amazon.com/dp/${asin}?tag=kethya08-20&linkCode=ll1&language=en_US`;
+  } else if (dest) {
+    // For non-Amazon links (Walmart, etc)
+    redirectUrl = decodeURIComponent(dest);
+  } else {
+    return new Response('Not found', { status: 404 });
+  }
+
+  return new Response(null, {
+    status: 302,
+    headers: {
+      'Location': redirectUrl,
+      'Cache-Control': 'no-cache, no-store, must-revalidate',
+    }
+  });
+};
+
+export const config = { path: '/go' };
