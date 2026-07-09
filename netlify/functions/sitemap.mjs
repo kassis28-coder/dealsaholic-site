d75078102aacf5530579dd7cb2f0b746a1de7471
kassis28-coder <kassis28@gmail.com>
2026-07-08 23:29:21 -0400

    Change sitemap function to return JSON response
    Refactored sitemap function to return JSON data instead of XML sitemap.

--- netlify/functions/sitemap.mjs
+++ netlify/functions/sitemap.mjs
@@ -1,46 +1,21 @@
 import { getStore } from "@netlify/blobs";
 
 export default async () => {
-  const baseUrl = "https://deals-aholic.com";
   const store = getStore("deals");
 
-  let data = await store.get("latest", { type: "json" });
+  const data = await store.get("latest", { type: "json" });
 
-  const staticPages = [
-    "",
-    "/submit.html",
-    "/about.html",
-    "/privacy.html"
-  ];
-
-  let urls = staticPages.map(page => `
-    <url>
-      <loc>${baseUrl}${page}</loc>
-      <changefreq>daily</changefreq>
-      <priority>0.8</priority>
-    </url>
-  `).join("");
-
-  if (data && data.deals && Array.isArray(data.deals)) {
-    data.deals.forEach(deal => {
-      urls += `
-      <url>
-        <loc>${baseUrl}/deal.html?asin=${deal.asin}</loc>
-        <lastmod>${deal.fetchedAt || new Date().toISOString()}</lastmod>
-        <changefreq>daily</changefreq>
-        <priority>0.7</priority>
-      </url>`;
-    });
-  }
-
-  const xml = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${urls}
-</urlset>`;
-
-  return new Response(xml, {
-    headers: {
-      "Content-Type": "application/xml"
+  return new Response(
+    JSON.stringify({
+      exists: !!data,
+      keys: data ? Object.keys(data) : [],
+      dealCount: data?.deals?.length || 0,
+      sample: data?.deals?.[0] || null
+    }, null, 2),
+    {
+      headers: {
+        "Content-Type": "application/json"
+      }
     }
-  });
+  );
 };
